1e0a74deb35bca61694e2f57a2c6d30df6a41d9e
Turinnash <istvangooz@gmail.com>
2020-12-12 14:03:25 +0100

    todo cont

---
+++ main.js
@@ -0,0 +1,54 @@
+(function(){
+
+
+    const bodyDay = document.querySelector('.body__day');
+    const bodyDate = document.querySelector('.body__date');
+    const dayNames = [
+               'Sunday',
+               'Monday',
+               'Tuesday', 
+               'Wednesday', 
+               'Thursday', 
+               'Friday', 
+               'Saturday'
+            ];
+
+    const localDB = {
+        setItem(key, value){
+            value= JSON.stringify(value);
+            localStorage.setItem(key, value);
+        },
+         getItem(key){
+             const value = localStorage.getItem(key);
+             if(!value){
+                 return null;
+             }
+             return JSON.parse(value);
+         },
+         removeItem(key) {
+             localStorage.removeItem(key);
+         }
+    };
+    const init = () => {
+        const savedTodos = localDB.getItem('todos');
+        if (savedTodos) {
+            todos = savedTodos;
+        }
+        showDate();
+    };
+    const showDate = () => {
+        const currentDate = new Date();
+        const day = [
+            currentDate.getFullYear(), 
+            currentDate.getMonth()+1,
+            currentDate.getDate()
+        ].map(num => num <10 ? `0${num}` : num);
+        bodyDay.textContent = dayNames[currentDate.getDay()];
+
+    };
+
+
+
+    init();
+
+})();
